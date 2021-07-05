b90dc4f87c19b875ee4ec95599f3372401efc4a4
Alexandro Perez <apgfriend@gmail.com>
2021-07-05 07:58:52 -0500

    Add a basic card component

---
+++ my-scope/ui/card/card.composition.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Card } from "./card";
+
+export const BasicCard = () => <Card text="hello from Card" />;
---
+++ my-scope/ui/card/card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./card.module.scss";
+import { Button, ButtonProps } from "@my-scope/ui.button";
+
+//                      This is a construct called intersection types
+//                      mainly used to combine existing object types.
+export type CardProps = ButtonProps & {
+  /** a text to be rendered in the component. */
+  text: string;
+  /** Text for button */
+  buttonText?: string;
+};
+
+export function Card({ text, importance, buttonText = "Learn More" }: CardProps) {
+  return (
+    <div className={styles.card}>
+      <h2>{text}</h2>
+      <Button importance={importance} text={buttonText} />
+    </div>
+  );
+}
